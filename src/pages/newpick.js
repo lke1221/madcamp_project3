982e54867de69ac3c266f3b8a7a174c0f3379c2e
yoontwo <61463592+yoontwo@users.noreply.github.com>
2021-07-20 14:43:43 +0900

    0720 14:43

--- src/pages/newpick.js
+++ src/pages/newpick.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import "./newnotice.css";
+//import "./newnotice.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import axios from "axios";
 import moment from "moment";
 
-const NewNotice = ({ history }) => {
+const NewPick = ({ history }) => {
   const [post, setPost] = useState({
     title: "",
     content: "",
@@ -50,32 +50,11 @@ const NewNotice = ({ history }) => {
         <h1>New Notice</h1>
         <div className="form-wrapper">
           <input
-            className="title-input"
+            style={{ width: 40 }}
+            className="input"
             type="text"
             placeholder="제목"
             onChange={getValue}
-          />
-          <CKEditor
-            editor={ClassicEditor}
-            data="내용을 입력하세요"
-            onReady={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
-            }}
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              console.log({ event, editor, data });
-              setPost({
-                ...post,
-                content: data,
-              });
-            }}
-            onBlur={(event, editor) => {
-              //console.log('Blur.', editor);
-            }}
-            onFocus={(event, editor) => {
-              //console.log('Focus.', editor);
-            }}
           />
         </div>
         <button className="submit-button" onClick={submitReview}>
@@ -86,4 +65,4 @@ const NewNotice = ({ history }) => {
   );
 };
 
-export default NewNotice;
+export default NewPick;
--- src/pages/pick.js
+++ src/pages/pick.js
@@ -1,16 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useState, Component } from "react";
 import PostList from "./PostList";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import firebaseInit from "../firebaseInit";
+//import PopUp from "./PopUp";
+import PopUpContent from "./PopUpContent";
+import ReactDom from "react-dom";
+import Popup from "reactjs-popup";
 
-const Pick = ({ history }) => {
+const Pick = ({ children }) => {
+  // constructor(props){
+  //   super(props);
+
+  //   this.state = {
+  //       isOpenPopup: false,
+  //   }
+
+  //   this.openPopup = this.openPopup.bind(this);
+  //   this.closePopup = this.closePopup.bind(this);
+  // }
+
+  const [isOpenPopup, setIsOpenPopup] = useState("false");
+
+  const openPopup = () => {
+    setIsOpenPopup("true");
+  };
+
+  const closePopup = () => {
+    setIsOpenPopup("false");
+  };
   return (
     <div
       style={{
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        // alignItems: "center",
+        flexDirection: "row",
         paddingTop: 80,
         //paddingLeft: 400,
         //paddingRight: 400
@@ -19,18 +44,29 @@ const Pick = ({ history }) => {
       <div style={{ width: 900 }}>
         <h1 style={{ marginBottom: 50, fontSize: 30 }}>역대 금주의 PICK!</h1>
 
-        <button
-          className="new-notice"
-          style={{ marginTop: 20, fontSize: 20 }}
-          onClick={
-            window.sessionStorage.getItem("position") === "admin"
-              ? () => history.push("/newnotice")
-              : () => alert("관리자만 글을 작성할 수 있습니다.")
-          }
-        >
-          Add
-        </button>
-        {/*</Link>*/}
+        {isOpenPopup ? (
+          <div style={{ alignItems: "center" }}>
+            <Popup
+              trigger={
+                <button
+                  className="new-notice"
+                  style={{ marginTop: 20, fontSize: 20 }}
+                  onClick={
+                    window.sessionStorage.getItem("position") === "admin"
+                      ? openPopup
+                      : () => alert("관리자만 글을 작성할 수 있습니다.")
+                  }
+                >
+                  Add
+                </button>
+              }
+            >
+              <PopUpContent />
+            </Popup>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
