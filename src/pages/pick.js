c46d6652df491d4d866701fd29ad353e6db8dc9b
yoontwo <61463592+yoontwo@users.noreply.github.com>
2021-07-20 15:13:24 +0900

    0720 15:13

--- src/pages/pick.js
+++ src/pages/pick.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React, { useState, Component } from "react";
-=======
-import React, { useEffect } from "react";
-import './newnotice.css'
->>>>>>> db130631d8bab67557176c2f9572bdcc87d1546e
+
 import PostList from "./PostList";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -46,10 +42,8 @@ const Pick = ({ children }) => {
         //paddingRight: 400
       }}
     >
-      <h1 style={{ marginBottom: 50, fontSize: 30 }}>역대 금주의 PICK!</h1>
-      <div style={{ width: 900, height: 450}}>
-
-<<<<<<< HEAD
+      <div style={{ width: 900, height: 450 }}>
+        <h1 style={{ marginBottom: 50, fontSize: 30 }}>역대 금주의 PICK!</h1>
         {isOpenPopup ? (
           <div style={{ alignItems: "center" }}>
             <Popup
@@ -74,21 +68,6 @@ const Pick = ({ children }) => {
           <></>
         )}
       </div>
-=======
-
-      </div>
-      <button
-          className="submit-button"
-          style={{ marginTop: 20, fontSize: 20 }}
-          onClick={
-            window.sessionStorage.getItem("position") === "admin"
-              ? () => history.push("/newnotice")
-              : () => alert("관리자만 글을 작성할 수 있습니다.")
-          }
-        >
-          Add
-      </button>
->>>>>>> db130631d8bab67557176c2f9572bdcc87d1546e
     </div>
   );
 };
